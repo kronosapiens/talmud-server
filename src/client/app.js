e5b50acc6b5ba311f7cd070348db08869614e1f8
Daniel Kronovet <kronovet@gmail.com>
2017-12-28 20:44:02 +0000

    Wire together Vue and Bootstrap

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,17 @@
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  }
+})
+
+const nav = new Vue({
+  el: '#nav',
+  data: {
+    title: 'Talmud',
+    pages: [
+      {text: 'Explore'},
+      {text: 'Vote'}
+    ]
   }
 })
