b1452f8cf65c7fe51a261f387cb720c592a449fb
Daniel Kronovet <kronovet@gmail.com>
2018-03-05 00:25:55 +0000

    Set explicit allowed CORS headers

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(passport.initialize())
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
   next()
 })
 
